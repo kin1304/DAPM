bad3f542bf73816cdc0da5c385b6dde5cf426074
kin1304 <nguyennha6a6kl@gmail.com>
2024-10-22 22:20:47 +0700

    add Package helpers and move loadfile from home to helper package

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ student_label = ["id", "Toán", "Văn", "Anh", "Lí", "Hóa", "Sinh", "Sử", "�
 student_37 = pd.read_csv('dataset/students_37.csv', header=None)
 student_37.columns = student_label
 
-student_37['Toán'].plot.hist(bins=10, color='skyblue', edgecolor='black')
-plt.title('Histogram of Age')
-plt.xlabel('Age')
-plt.ylabel('Frequency')
-plt.savefig('histogram.png')
-img = Image.open('histogram.png')
-st.image(img, caption="Ảnh trực quan điểm toán")
 
+fig, ax = plt.subplots()
+student_37['Toán'].plot.hist(bins=10, color='skyblue', edgecolor='black', ax=ax)
+ax.set_title('Histogram of Age')
+ax.set_xlabel('Score')
+ax.set_ylabel('Frequency')
+# plt.savefig('histogram.png')
+# img = Image.open('histogram.png')
+# st.image(img, caption="Ảnh trực quan điểm toán")
 
+st.pyplot(fig)
 
 
