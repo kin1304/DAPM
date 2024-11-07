3fc5fef962bb1a1744bd514f32339011e2c4b248
kin1304 <nguyennha6a6kl@gmail.com>
2024-11-07 23:32:01 +0700

    fix file loadfile and and groupby to file read_dict

--- Helpers/AlgorithmHelpers/read_dict.py
+++ Helpers/AlgorithmHelpers/read_dict.py
@@ -1,11 +1,18 @@
 import numpy as np
+import pandas as pd
+import streamlit as st
 
-def read(data:dict):
-    all_names = [transaction["names"] for transaction in data.values()]
-    all_utilities = [transaction["utilities"] for transaction in data.values()]
 
+def read(data: dict):
+    names = []
+    utilities = []
+    for key, value in data.items():
+        df = pd.DataFrame(value)
+        df["utilities"].astype(float)
+        df = df.groupby("names", as_index=False).sum()
+        names.append(df["names"].values)
+        utilities.append(df["utilities"].values)
     # Chuyển đổi thành các mảng đa chiều (số transaction x số name trong mỗi transaction)
-    names_array = np.array(all_names, dtype=object)
-    utilities_array = np.array(all_utilities, dtype=object)
-
+    names_array = np.array(names, dtype=object)
+    utilities_array = np.array(utilities, dtype=object)
     return names_array, utilities_array
