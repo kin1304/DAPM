ab02d732c5da16b8c5b99f72157410b849c4e724
kin1304 <nguyennha6a6kl@gmail.com>
2024-11-08 15:28:43 +0700

    finnish HUI_Miner

--- Helpers/AlgorithmHelpers/read_dict.py
+++ Helpers/AlgorithmHelpers/read_dict.py
@@ -1,14 +1,15 @@
+from typing import Any
 import numpy as np
 import pandas as pd
-import streamlit as st
+from numpy import ndarray, dtype
 
 
-def read(data: dict):
+def read(data: dict) -> tuple[ndarray[Any, dtype[Any]], ndarray[Any, dtype[Any]]]:
     names = []
     utilities = []
     for key, value in data.items():
         df = pd.DataFrame(value)
-        df["utilities"].astype(float)
+        df["utilities"] = df["utilities"].astype(float)
         df = df.groupby("names", as_index=False).sum()
         names.append(df["names"].values)
         utilities.append(df["utilities"].values)
